2d55545eccf0a64c8c2b9025064ea83672cd5024
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2020-12-09 14:14:34 -0300

    fix: remove database from Client

--- clients/kael/src/interfaces/all/client.ts
+++ clients/kael/src/interfaces/all/client.ts
@@ -1,10 +1,8 @@
-import { KaelDatabase } from '@kaelbot/database';
 import { Collection, Client as DiscordJSClient } from 'discord.js';
 
 import { Event } from '../structures/abstract/event';
 
 export interface Client extends DiscordJSClient {
-  database: KaelDatabase;
   events: Collection<string, Event>;
   connect(): Promise<string>;
 }
